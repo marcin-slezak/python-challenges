88901d6b13d4cddd65d25abb3d3c5b46dd73d932
Marcin Ślęzak <35666945+marcin-slezak@users.noreply.github.com>
2026-02-04 18:39:17 +0100

    Create challenge-1.py

---
+++ challenge-1.py
@@ -0,0 +1,20 @@
+
+teams = [
+    {"name": "Warsaw Eagles", "founded": 2018, "players": [
+        {"nick": "NeoFox", "rating": 1.12},
+        {"nick": "Kiro", "rating": 1.05},
+        {"nick": "Vex", "rating": 1.18},
+        {"nick": "Luna", "rating": 0.98},
+        {"nick": "Sparks", "rating": 1.01},
+    ]},
+    {"name": "Gdansk Phantoms", "founded": 2015, "players": [
+        {"nick": "Raze", "rating": 1.22},
+        {"nick": "Milo", "rating": 1.08},
+        {"nick": "Snipe", "rating": 1.10},
+        {"nick": "Keen", "rating": 1.06},
+        {"nick": "Odin", "rating": 0.97},
+    ]},
+    {"name": "Krakow Wolves", "founded": 2021, "players": []},  # brak danych
+]
+
+# Cel: policz średni rating drużyny na podstawie ratingów graczy, posortuj malejąco, pokaż ranking
