7158324e0d7b4701197cf1effad18444ca62d5e6
Marcin Ślęzak <35666945+marcin-slezak@users.noreply.github.com>
2026-02-04 18:43:27 +0100

    Create challenge-3.py

---
+++ challenge-3.py
@@ -0,0 +1,17 @@
+
+teams = [
+    {"id": "WE", "name": "Warsaw Eagles"},
+    {"id": "GP", "name": "Gdansk Phantoms"},
+    {"id": "KW", "name": "Krakow Wolves"},
+    {"id": "PP", "name": "Poznan Panthers"},
+]
+
+matches = [
+    {"date": "2025-10-02", "teamA": "WE", "teamB": "GP", "mapsA": 2, "mapsB": 1},
+    {"date": "2025-10-05", "teamA": "KW", "teamB": "PP", "mapsA": 0, "mapsB": 2},
+    {"date": "2025-10-10", "teamA": "WE", "teamB": "PP", "mapsA": 2, "mapsB": 0},
+    {"date": "2025-10-12", "teamA": "GP", "teamB": "KW", "mapsA": 2, "mapsB": 1},
+    {"date": "2025-10-20", "teamA": "WE", "teamB": "KW", "mapsA": 1, "mapsB": 2},
+]
+
+# Cel: na podstawie listy meczów policz bilans W/L dla drużyn, posortuj tabelę, pokaż top 2, i wypisz mecze jednej drużyny.
