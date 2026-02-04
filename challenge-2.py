fb2d490c390216b22507deb6be0921f9bdb653f8
Marcin Ślęzak <35666945+marcin-slezak@users.noreply.github.com>
2026-02-04 18:42:20 +0100

    Create challenge-2.py

---
+++ challenge-2.py
@@ -0,0 +1,9 @@
+tournaments = [
+    {"name": "Katowice Clash", "type": "LAN", "maps_played": 12, "maps_won": 8, "rounds_won": 210},
+    {"name": "Online Open #7", "type": "ONLINE", "maps_played": 10, "maps_won": 6, "rounds_won": 175},
+    {"name": "Berlin Major Quals", "type": "LAN", "maps_played": 14, "maps_won": 9, "rounds_won": 242},
+    {"name": "EU Cup", "type": "ONLINE", "maps_played": 8, "maps_won": 3, "rounds_won": 120},
+]
+
+
+# Cel: wybierz tylko turnieje LAN, policz łączną liczbę map, winrate, oraz średnią rund na mapę.
